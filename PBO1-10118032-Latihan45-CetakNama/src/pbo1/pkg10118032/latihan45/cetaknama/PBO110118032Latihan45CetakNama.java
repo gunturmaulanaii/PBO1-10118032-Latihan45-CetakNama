769d0f6792268ed39e957b01a89769a56f16a3d6
Guntur Maulana Ibrahim <55784684+gunturmaulanaii@users.noreply.github.com>
2019-10-27 19:07:59 +0700

    Add files via upload

---
+++ PBO1-10118032-Latihan45-CetakNama/src/pbo1/pkg10118032/latihan45/cetaknama/PBO110118032Latihan45CetakNama.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118032.latihan45.cetaknama;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Guntur Maulana I
+ * NAMA  : Guntur Maulana Ibrahim
+ * KELAS : IF 1
+ * NIM   : 10118032
+ * Deskripsi Program : Menampilkan output Cetak Nama.
+ */
+public class PBO110118032Latihan45CetakNama {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner scn = new Scanner(System.in);
+        Cetak print = new Cetak();
+        
+        System.out.println("=====Aplikasi Pencetak Nama=====");
+        System.out.print("Masukkan nama anda : ");
+        print.setNama(scn.nextLine());
+        System.out.print("Mau cetak nama berapa kali? : ");
+        print.setJmlCetak(scn.nextInt());
+        
+        print.cetak(print.getNama());
+        print.cetak(print.getJmlCetak(), print.getNama());
+    }
+   
+}
